97886b9f789e8f50f90601343e34e192639b99a2
Ben Anhalt <ben.anhalt@gmail.com>
2024-03-15 12:37:21 -0500

    Remove dependence on hmatrix.

--- day24/solution.hs
+++ day24/solution.hs
@@ -1,7 +1,4 @@
 import Data.List (tails)
-import Data.List.Split (chunksOf)
-import Numeric.LinearAlgebra
-import Numeric.LinearAlgebra.Data
 
 pairs :: [a] -> [(a, a)]
 pairs l = [(x, y) | (x : ys) <- tails l, y <- ys]
@@ -19,36 +16,43 @@ test :: (Ord a, Num a) => a -> a -> Maybe (a, a, a, a) -> Bool
 test _ _ Nothing = False
 test a b (Just (t, t', x, y)) = t >= 0 && t' >= 0 && x >= a && x <= b && y >= a && y <= b
 
-crossV :: Vector Z -> Matrix Z
-crossV v =
-  let
-    [x, y, z] = toList v
-  in
-    (3><3)
-    [ 0, -z,  y,
-      z,  0, -x,
-     -y,  x,  0 ]
+partOne :: Real a => [[a]] -> Int
+partOne stones = length $ filter (test 200000000000000 400000000000000) $
+  map (uncurry intersect) $ pairs $ map toRational <$> stones
 
-solve :: [[Z]] -> [Rational]
+crossV :: Num a => [a] -> [[a]]
+crossV [x, y, z] =
+    [[ 0, -z,  y],
+     [ z,  0, -x],
+     [-y,  x,  0]]
+
+mMult :: Num a => [[a]] -> [a] -> [a]
+mMult m v = map (sum . zipWith (*) v) m
+
+vSub :: Num a => [a] -> [a] -> [a]
+vSub = zipWith (-)
+
+joinHorz :: [[a]] -> [[a]] -> [[a]]
+joinHorz = zipWith (++)
+
+joinVert :: [a] -> [a] -> [a]
+joinVert = (++)
+
+solve :: [[Rational]] -> [Rational]
 solve [s1, s2, s3] =
-  let r1 = fromList $ take 3 s1
-      r2 = fromList $ take 3 s2
-      r3 = fromList $ take 3 s3
-      v1 = fromList $ drop 3 s1
-      v2 = fromList $ drop 3 s2
-      v3 = fromList $ drop 3 s3
-      rhs =
-        map toRational $ toList $ vjoin
-          [ (v1 `cross` r1) - (v2 `cross` r2),
-            (v1 `cross` r1) - (v3 `cross` r3)
-          ]
-      coeff :: [[Rational]] = map (map toRational) $ toLists $ fromBlocks
-        [[crossV (v1 - v2), crossV (r1 - r2)],
-         [crossV (v1 - v3), crossV (r1 - r3)]]
-   in gauss coeff rhs
+  let (r1, v1) = splitAt 3 s1
+      (r2, v2) = splitAt 3 s2
+      (r3, v3) = splitAt 3 s3
+      rhs = joinVert
+        ((crossV v1 `mMult` r1) `vSub` (crossV v2 `mMult` r2))
+        ((crossV v1 `mMult` r1) `vSub` (crossV v3 `mMult` r3))
+      coeff = joinVert
+        (joinHorz (crossV $ v1 `vSub` v2) (crossV $ r1 `vSub` r2))
+        (joinHorz (crossV $ v1 `vSub` v3) (crossV $ r1 `vSub` r3))
+   in geSolve coeff rhs
 
-gauss :: (Fractional a, Eq a) => [[a]] -> [a] -> [a]
-gauss a b = resubstitute $ triangular a'
+geSolve :: (Fractional a, Eq a) => [[a]] -> [a] -> [a]
+geSolve a b = resubstitute $ triangular a'
   where
     a' = zipWith (++) a $ map pure b
 
@@ -58,8 +62,8 @@ triangular m = row : triangular rows'
   where
     (row : rows) = rotatePivot m
     rows' = map f rows
-    f bs | (head bs) == 0 = drop 1 bs
-         | otherwise      = drop 1 $ zipWith (-) (map (*c) bs) row
+    f bs | head bs == 0 = drop 1 bs
+         | otherwise    = drop 1 $ zipWith (-) (map (*c) bs) row
            where c = (head row)/(head bs)
     rotatePivot m@(row : rows) | head row /= 0 = m
                                | otherwise     = rotatePivot (rows ++ [row])
@@ -74,11 +78,15 @@ resubstitute = reverse . resubstitute' . reverse . map reverse
         rows' = map substituteUnknown rows
         substituteUnknown (a1:a2:as) = a1-x*a2:as
 
+partTwo :: Real a => [[a]] -> Rational
+partTwo stones =
+  let solution = solve $ map (map toRational) $ take 3 stones
+  in sum $ take 3 solution
+
 main :: IO ()
 main = do
   input <- lines <$> readFile "input.txt"
   let stones = map read . words . filter (`elem` "0123456789 -") <$> input
-  print $ length $ filter (test 200000000000000 400000000000000) $ map (uncurry intersect) $ pairs $ map toRational <$> stones
-  let solution =  solve $ take 3 stones
-  print $ sum $ take 3 solution
+  print $ partOne stones
+  print $ partTwo stones
 
